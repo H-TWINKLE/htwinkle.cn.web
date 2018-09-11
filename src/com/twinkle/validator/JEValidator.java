9560ae299cb8372ddf8e2ad02137e6447de7cfcb
TWINKLE <TWINKLE@TWINKLE>
2018-09-11 16:50:16 +0800

    修改login bug

--- src/com/twinkle/validator/JEValidator.java
+++ src/com/twinkle/validator/JEValidator.java
@@ -7,25 +7,27 @@ public class JEValidator extends Validator {
 
 	@Override
 	protected void validate(Controller c) {
-		
+
+		this.setShortCircuit(true);
+
 		validateRequiredString("admin", "adminMsg", "请输入学号");
 		validateRequiredString("pass", "passMsg", "请输入密码");
-		
-		
 		validateString("admin", 12, 12, "adminMsg", "请输入12位学号喔!");
-		validateString("pass", 6, 100, datePattern, "密码最低为6位!");;
+		validateString("pass", 6, 100, "passMsg", "密码最低为6位!");
+		
 
 	}
 
 	@Override
 	protected void handleError(Controller c) {
-		
+
 		String actionKey = getActionKey();
-		
+
+
 		if (actionKey.equals("/jwgl/login"))
 			controller.render("/pages/page_jwgl.html");
-		
-		else if(actionKey.equals("/eol/login"))
+
+		else if (actionKey.equals("/eol/login"))
 			controller.render("/pages/page_eol.html");
 	}
 
