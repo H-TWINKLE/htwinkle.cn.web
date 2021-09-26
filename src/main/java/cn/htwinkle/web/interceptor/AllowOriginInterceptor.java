8255fd6b2a0cf1019f75dc7cb3ca4cd7ab60949c
TWINKLE <zhanghjqqqq@163.com>
2021-09-26 20:50:33 +0800

    1.新增跨域api接口

--- src/main/java/cn/htwinkle/web/interceptor/AllowOriginInterceptor.java
+++ src/main/java/cn/htwinkle/web/interceptor/AllowOriginInterceptor.java
@@ -6,18 +6,18 @@ import com.jfinal.core.Controller;
 
 public class AllowOriginInterceptor implements Interceptor {
 
-	@Override
-	public void intercept(Invocation arg0) {
+    @Override
+    public void intercept(Invocation arg0) {
 
-		Controller c = arg0.getController();
-		c.getResponse().setHeader("Access-Control-Allow-Origin", "*");
-		c.getResponse().setHeader("Access-Control-Allow-Methods", "*");
-		c.getResponse().setHeader("Access-Control-Max-Age", "3600");
-		c.getResponse().setHeader("Access-Control-Allow-Headers",
-				"Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+        Controller c = arg0.getController();
+        c.getResponse().setHeader("Access-Control-Allow-Origin", "*");
+        c.getResponse().setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS, DELETE, PUT");
+        c.getResponse().setHeader("Access-Control-Max-Age", "3600");
+        c.getResponse().setHeader("Access-Control-Allow-Headers",
+                "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 
-		arg0.invoke();
+        arg0.invoke();
 
-	}
+    }
 
 }
